7c17a15101355879f2b68ba237828eeafdd13750
Brian Ketelsen <bketelsen@gmail.com>
2011-04-11 16:10:06 -0400

    fuck logging

--- bkweb.go
+++ bkweb.go
@@ -2,20 +2,12 @@ package main
 
 import (
     "github.com/hoisie/web.go"
-		"os"
-	"log"
 )
 
 func hello(val string) string { return "hello " + val } 
 
 func main() {
-	f, err := os.Open("bkweb-server.log", os.O_RDWR|os.O_CREATE, 0644)
-    if err != nil {
-        println(err.String())
-        return
-    }
-    logger := log.New(f, "", log.Ldate|log.Ltime)
-    web.Get("/hello(.*)", hello)
-	web.SetLogger(logger)
+  web.Get("/hello(.*)", hello)
+
     web.Run("0.0.0.0:9999")
 }
